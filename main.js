1aecbe586178abe365e84b1d63b6783db660df42
foxtrot2609 <73159713+foxtrot2609@users.noreply.github.com>
2021-01-29 17:23:21 +0300

    feat: add js code, const and let

---
+++ main.js
@@ -0,0 +1,26 @@
+const app = () => {
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
+    
+    // Sounds 
+    const sounds = document.querySelectorAll(".sound-picker button");
+    // Time display
+    const timeDisplay = document.querySelector(".time-display");
+    // Get the length of outline
+    const outlineLength = outline.getTotalLength();
+    // Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // Play sound
+    play.addEventListener("click", () => {
+        song.play();
+    });
+};
+
+
+app();
